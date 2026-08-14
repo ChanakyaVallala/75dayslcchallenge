29b9c4d183122d439ced1684bef5d3feb5629799
ChanakyaVallala <144151210+ChanakyaVallala@users.noreply.github.com>
2026-08-15 01:06:25 +0530

    Time: 7 ms (75.87%), Space: 16 MB (95.8%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -1,42 +1,58 @@
-from collections import deque
-
-class Solution:
-    def solve(self, board):
-        if not board:
-            return
-        rows = len(board)
-        cols = len(board[0])
-        q = deque()
-        for r in range(rows):
-            if board[r][0] == 'O':
-                q.append((r, 0))
-                board[r][0] = '#'
-
-            if board[r][cols - 1] == 'O':
-                q.append((r, cols - 1))
-                board[r][cols - 1] = '#'
-
-        for c in range(cols):
-            if board[0][c] == 'O':
-                q.append((0, c))
-                board[0][c] = '#'
-            if board[rows - 1][c] == 'O':
-                q.append((rows - 1, c))
-                board[rows - 1][c] = '#'
-        directions = [(1,0),(-1,0),(0,1),(0,-1)]
+class Solution(object):
+    def bfs(self,q,board):
+        ROWS = len(board)
+        COLS = len(board[0])
+        dns = [(0,1),(1,0),(-1,0),(0,-1)]
         while q:
-            r, c = q.popleft()
-            for dr, dc in directions:
-                nr = r + dr
-                nc = c + dc
-                if (0 <= nr < rows and
-                    0 <= nc < cols and
-                    board[nr][nc] == 'O'):
-                    board[nr][nc] = '#'
-                    q.append((nr, nc))
-        for r in range(rows):
-            for c in range(cols):
-                if board[r][c] == 'O':
-                    board[r][c] = 'X'
-                elif board[r][c] == '#':
-                    board[r][c] = 'O'
+            r,c = q.popleft()
+            for dr,dc in dns:
+                i = r + dr
+                j = c + dc
+                # strictly out of bounds mean one O value is in the boundaries
+                if(i<0 or i==ROWS or j<0 or j==COLS or board[i][j]=="T"):
+                    continue
+                if(board[i][j]=="O"):
+                    board[i][j]="T"
+                    q.append((i,j))
+        return     
+
+
+    def solve(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: None Do not return anything, modify board in-place instead.
+        """
+        from collections import deque
+        ROWS = len(board)
+        COLS = len(board[0])
+        visited = set()
+        q = deque()
+        for i in range(ROWS):
+            if(board[i][0]=="O"):
+                q.append((i,0))
+                board[i][0]="T"
+                self.bfs(q,board)
+            if(board[i][COLS-1]=="O"):
+                q.append((i,COLS-1))    
+                board[i][COLS-1]="T"
+                self.bfs(q,board)
+        for j in range(COLS):
+            if(board[0][j]=="O"):
+                q.append((0,j))
+                board[0][j]="T"
+                self.bfs(q,board)
+            if(board[ROWS-1][j]=="O"):
+                q.append((ROWS-1,j))
+                board[ROWS-1][j]="T"
+                self.bfs(q,board)  
+            
+        for i in range(ROWS):
+            for j in range(COLS):
+                if board[i][j]=="O":
+                    board[i][j]="X"
+                elif board[i][j]=="T":
+                    board[i][j]="O"
+        
+        return
+        
+        
